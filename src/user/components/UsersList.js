abd694dfa6d23ea2f1797e37dd3b9728018cb853
ryderjenkins <ryderjenkins11@gmail.com>
2019-12-17 12:03:16 +0000

    Styled components

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import UserItem from './UserItem';
-import './UserList.css'
+import './UsersList.css'
 
 const UsersList = (props) => {
     if (props.items.length === 0) {
