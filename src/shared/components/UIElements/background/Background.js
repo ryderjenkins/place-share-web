b3eca8db6f6b747742a0db1a75da97f004010351
ryderjenkins <ryderjenkins11@gmail.com>
2019-12-17 15:03:20 +0000

    Remove portal

--- src/shared/components/UIElements/background/Background.js
+++ src/shared/components/UIElements/background/Background.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import './Background.css';
 
 const Background = props => {
-  return ReactDOM.createPortal(
-    <div className="background" onClick={props.onClick}></div>,
-    document.getElementById('background-hook')
-  );
-};
+  return (
+    <div className="background" onClick={props.onClick}></div>
+  )
+}
 
 export default Background;
