49daba7d07b396206e6a8d44db89897475aa2a35
ryderjenkins <ryderjenkins11@gmail.com>
2019-12-18 12:09:04 +0000

    New place form progressing

--- src/shared/components/FormElements/input/Input.js
+++ src/shared/components/FormElements/input/Input.js
@@ -3,9 +3,23 @@ import React from 'react';
 import './Input.css';
 
 const Input = (props) => {
-    return (
-        <div></div>
-    )
+  const el = props.element === 'input' 
+    ? (<input 
+        id={props.id} 
+        type={props.type} 
+        placeholder={props.placeholder}
+    />)
+    : (<textarea 
+        id={props.id} 
+        rows={props.rows || 3}
+    />);
+
+  return (
+      <div className={`form-control`}>
+        <label htmlFor={props.id}>{props.label}</label>
+          {el}
+      </div>
+  )
 }
 
 export default Input;
