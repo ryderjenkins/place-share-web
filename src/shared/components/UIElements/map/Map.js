cb93db4688e13803f43d64e8714f2f10611a3289
ryderjenkins <ryderjenkins11@gmail.com>
2019-12-18 11:14:02 +0000

    calling Google Maps

--- src/shared/components/UIElements/map/Map.js
+++ src/shared/components/UIElements/map/Map.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 import './Map.css';
 
 const Map = (props) => {
+  const mapRef = useRef();
+  const map = new window.google.maps.Map(mapRef.current, 
+    {
+      center: props.center,
+      zoom: props.zoom
+    }
+  )
+
+  new window.google.Maps.Marker(
+    {
+      position: props.center,
+      map: map
+    }
+  )
+
   return (
-    <div className={`map ${props.className}`} style={props.style}>
+    <div ref={mapRef} className={`map ${props.className}`} style={props.style}>
       
     </div>
   );
