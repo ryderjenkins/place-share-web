51811507201a86f6a222843bbd79821ec5a0c97e
ryderjenkins <ryderjenkins11@gmail.com>
2020-01-03 15:02:19 +0000

    Error handling

--- src/user/pages/Authentication.js
+++ src/user/pages/Authentication.js
@@ -3,6 +3,8 @@ import React, { useState, useContext } from 'react';
 import Input from '../../shared/components/FormElements/input/Input';
 import Card from '../../shared/components/UIElements/card/Card';
 import Button from '../../shared/components/FormElements/button/Button';
+import ErrorModal from '../../shared/components/UIElements/modal/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/loadingSpinner/LoadingSpinner';
 import { useForm } from '../../shared/hooks/form';
 import { AuthenticationContext } from '../../shared/context/authentication-context';
 import { VALIDATOR_EMAIL, VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/utils/validators';
@@ -85,47 +87,57 @@ const Authentication = () => {
     }
   };
 
+  const handleError = () => {
+    setError(null);
+  };
+
   return (
-    <Card className="authentication">
-      <h2>Login Required</h2>
-      <form onSubmit={submitAuthentication}>
-        {!isLoginForm && (
+    <>
+      <ErrorModal error={error} onClear={handleError} />
+      <Card className="authentication">
+        {isLoading && (
+          <LoadingSpinner asOverlay />
+        )}
+        <h2>Login Required</h2>
+        <form onSubmit={submitAuthentication}>
+          {!isLoginForm && (
+            <Input
+              id="name"
+              elementType="input"
+              type="text"
+              label="Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorMessage="Please enter a name"
+              onInput={inputChange}
+            />
+          )}
           <Input
-            id="name"
+            id="email"
             elementType="input"
-            type="text"
-            label="Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorMessage="Please enter a name"
+            type="email"
+            label="E-Mail Address"
+            validators={[VALIDATOR_EMAIL()]}
+            errorMessage="Please enter a valid email address."
             onInput={inputChange}
           />
-        )}
-        <Input
-          id="email"
-          elementType="input"
-          type="email"
-          label="E-Mail Address"
-          validators={[VALIDATOR_EMAIL()]}
-          errorMessage="Please enter a valid email address."
-          onInput={inputChange}
-        />
-        <Input
-          id="password"
-          elementType="input"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorMessage="Please enter a valid password, at least 5 characters."
-          onInput={inputChange}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginForm ? 'LOGIN' : 'SIGNUP'}
+          <Input
+            id="password"
+            elementType="input"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorMessage="Please enter a valid password, at least 5 characters."
+            onInput={inputChange}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginForm ? 'LOGIN' : 'SIGNUP'}
+          </Button>
+        </form>
+        <Button inverse onClick={switchForm}>
+          {isLoginForm ? 'Signup Instead' : 'Login'}
         </Button>
-      </form>
-      <Button inverse onClick={switchForm}>
-        {isLoginForm ? 'Signup Instead' : 'Login'}
-      </Button>
-    </Card>
+      </Card>
+    </>
   );
 };
 
